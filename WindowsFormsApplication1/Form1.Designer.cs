9175fcdd9d4fbc0a5981d4482de6834558190d1d
sanjayr <sanjayranjeeth@gmail.com>
2020-02-11 17:08:41 +0200

    Label 2 Updated by Member sanjayr who is member 2 and does not own the Repo

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -48,9 +48,9 @@
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(478, 271);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(209, 64);
+            this.label2.Size = new System.Drawing.Size(618, 64);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Hello 2";
+            this.label2.Text = "Hello 2 from Member 2";
             // 
             // Form1
             // 
